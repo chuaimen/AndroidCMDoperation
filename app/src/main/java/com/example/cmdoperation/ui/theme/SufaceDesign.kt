b416bbdc4d8e6ff1d6ce861de764860540245dd9
mac <chuaimen@163.com>
2024-12-29 19:46:26 +0800

    Initial commit

--- app/src/main/java/com/example/cmdoperation/ui/theme/SufaceDesign.kt
+++ app/src/main/java/com/example/cmdoperation/ui/theme/SufaceDesign.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 

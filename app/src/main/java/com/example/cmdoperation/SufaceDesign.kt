a1b6c6a06f065db9cb2d8daa2d093f4c1f3066ce
mac <chuaimen@163.com>
2024-12-30 14:15:59 +0800

    修改 页面布局的细节 修改 mian页面 对CMDpage 进行应用

--- app/src/main/java/com/example/cmdoperation/SufaceDesign.kt
+++ app/src/main/java/com/example/cmdoperation/SufaceDesign.kt
@@ -5,31 +5,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 
 @Composable
-fun CMDpage(
-){
+fun CMDpage(modifier: Modifier){
     Column(modifier = Modifier
-        //.fillMaxSize()
+        .fillMaxSize()
         .padding(5.dp)
     ) {
         Row(Modifier
             .height(720.dp)
             .fillMaxWidth()
-            .background(color = Color.Black)
         ) {
             Text("gg")
         }
@@ -43,10 +44,17 @@ fun CMDpage(
                 }
 
         }
-        Spacer(Modifier.padding(5.dp))
-        Row{
+        Spacer(Modifier.padding(2.dp))
+        Row(){
             OutlinedTextField(value = "test", onValueChange = {})
         }
+        Spacer(Modifier.padding(10.dp))
 
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CMDpagePreview(){
+    CMDpage(modifier = Modifier)
 }
